c3eb682194c97fe8211ccb708792d56b74f6d5c4
THARUUX <tdamsara6@gmail.com>
2024-12-13 04:27:44 +0530

    animations added

---
+++ typewriter/settings.js
@@ -0,0 +1,30 @@
+/**
+ * settings.js 
+ *
+ * Copyright © Sooraj Gupta 2020
+ *
+ * Change these values to affect
+ * default settings
+ */
+
+var settings = 
+{
+	//Default font-family
+	"font": "Urbanist",
+	
+	//Default font size;
+	"fontsize": "",
+	
+	//Default Font weight
+	"fontweight": "",
+	
+	//Time to when wait at end of typing before deleting
+	"timer": 20,
+	
+	//delay between typing each letter
+	"delay": 40,
+	
+	//cursor blink time
+	"cursor": 500
+	
+};
